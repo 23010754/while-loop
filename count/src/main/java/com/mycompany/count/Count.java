6cc3afb8ad6f5529c73fa4bd4e842e5c8cda5f2a
23010754 vhuhwavho netshitangani <23010754@mvula.univen.ac.za>
2025-04-28 23:30:44 +0200

    count

---
+++ count/src/main/java/com/mycompany/count/Count.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.count;
+
+/**
+ *
+ * @author admin
+ */
+public class Count {
+
+    public static void main(String[] args) {
+        
+        
+        int num1=0;
+        while(num1<=1000){
+          
+            System.out.println(num1+"Hello World!");
+            num1++;
+        } 
+       
+    }
+}
